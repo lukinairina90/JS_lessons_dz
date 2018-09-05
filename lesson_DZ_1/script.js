0d852f6716ccd85cd956923cfe48006ee4070199
Irina <irinalukina90@gmail.com>
2018-09-05 21:27:37 +0300

    Add all files

---
+++ lesson_DZ_1/script.js
@@ -0,0 +1,40 @@
+// // Объявление переменных
+//   var LeftBorderWidth = 1;
+//   const pi = 3.14;
+//   {
+//       let second = 2;
+//   }
+//
+//
+// // 6 типов двнных (с новым стандартом 7)
+// var number = 5;
+// var string = "Hello";
+// var sym = Sumbol();
+// var boolean = true;
+// var obj = {};
+// null;        // когда в коде чего-то просто не существует
+// undefined;   // объект который существует, но никакого значения не имеет
+// //Специальные числовые значения
+// infinity   //когда делим на ноль (0)
+// NaN     // операции которые не соответствуют математической логике
+
+
+  obj = {
+    name: "Joe face",
+    age: 25,
+    isMarried: true
+  }
+
+console.log(obj.name);
+console.log(obj['name']);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[0]);
+
+alert('HELLO!');
+confirm('are you here?');
+prompt('Вам есть 19?', '19');
+
+var answer = prompt('Вам есть 19?', '19');
+
+console.log( typeof(answer));
